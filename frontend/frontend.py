ccae421ddc383dfb690f073ebfd9b7604af9c2f0
gitch23 <128653704+gitch23@users.noreply.github.com>
2025-04-22 17:24:14 -0500

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,11 +1,19 @@
 import requests
+import time
 
 backend_url = "http://backend-container:5000"
 
-response = requests.get(backend_url)
+while True:
+    try:
+        response = requests.get(backend_url)
+        if response.status_code == 200:
+            print("Frontend received the following response from the backend:")
+            print(response.text)
+        else:
+            print("Failed to communicate with the backend.")
+    except Exception as e:
+        print("An error occured" , e)
+    time.sleep(10)
 
-while response.status_code == 200:
-    print("Frontend received the following response from the backend:")
-    print(response.text)
-else:
-    print("Failed to communicate with the backend.")
+        
+
