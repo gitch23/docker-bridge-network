79b5db3a25d3ae892f05434e531f0d77633ce55e
gitch23 <128653704+gitch23@users.noreply.github.com>
2025-04-22 17:09:19 -0500

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -4,7 +4,7 @@ backend_url = "http://backend-container:5000"
 
 response = requests.get(backend_url)
 
-if response.status_code == 200:
+while response.status_code == 200:
     print("Frontend received the following response from the backend:")
     print(response.text)
 else:
